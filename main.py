a14135dd3b51d81839f8c48b246836888bf56cf5
Maressay <yasseravalosmontero14@gmail.com>
2025-07-05 23:46:43 -0500

    implemented rag with llm, pd: i need improve the faiss

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from transformers.utils.quantization_config import BitsAndBytesConfig
 import torch
 
+# imports for RAG
+from langchain_huggingface import HuggingFaceEmbeddings # para crear y leer embeddings
+from langchain_community.vectorstores import FAISS # la base de datos vectorial a usar es FAISS
+
+# load embeddings
+embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+vectorstore = FAISS.load_local("./data/data_horarios", embeddings = embedding, allow_dangerous_deserialization=True)
+retriever = vectorstore.as_retriever(sarch_kwargs={"k": 3})
+
+# load model
 quant_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,
@@ -13,24 +23,50 @@ model_id = "microsoft/Phi-3-mini-128k-instruct"
 
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
-    device_map="auto",
+    device_map="cuda",
     quantization_config=quant_config,
     torch_dtype=torch.float16
 )
 
-tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True, max_length=1000)
+tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True, max_length=500)
 
-prompt = 'Cual es la capital del peru?'
+pipe = pipeline('text-generation', model=model, tokenizer=tokenizer, return_full_text=False)
 
-inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
+# input user
 
-with torch.no_grad():
-    output = model.generate(
-        **inputs,
-        max_new_tokens=500,
-        temperature=0.1,
-        top_p=0.9,
-        do_sample=True
+input_user = input('You 🥸: ')
+
+# get context from FAISS
+
+docs = retriever.invoke(input_user)
+context = "\n".join([doc.page_content for doc in docs])
+
+# building prompt
+
+prompt = f"""
+<|system|>
+Eres un asistente académico que responde preguntas sobre los horarios de clases de los alumnos.
+Tu única fuente de información es el contexto proporcionado. No inventes respuestas. Si no sabes algo, responde educadamente que no puedes ayudar.
+<|end|>
+<|user|>
+Contexto:
+
+{context}
+
+Pregunta del usuario: {input_user}
+<|end|>
+<|assistant|>
+"""
+
+# generate answer
+
+output = pipe(
+    prompt,
+    max_new_tokens=300,
+    temperature=0.1,
+    do_sample=False,
+    return_full_text=False
     )
 
-    print(tokenizer.decode(output[0], skip_special_tokens=True))
+print('Model 🤖: ', output[0]["generated_text"])
+
